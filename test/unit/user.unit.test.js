12e1ad3e655cb8dfc19027e2a9fc4a8a554f681e
Jonathan Fernandes Benevides <jfernabe@everis.com>
2020-03-13 15:20:46 -0300

    test

--- test/unit/user.unit.test.js
+++ test/unit/user.unit.test.js
@@ -1,20 +1,33 @@
-const repo = require('../../src/core/repo/user.repo')
+require("dotenv/config");
+const repo = require("../../src/core/repo/user.repo");
+const db = require("../../src/core/db")(process.env.MONGO_URL);
+describe("testando o repositório do usuário", () => {
 
-describe('testando o repositório do usuário', () =>{
-    it('listar usuários', async () => {
-        const result = await repo.list();
+  it("list users", async () => {
+    // db.connect().then(() => {
+      repo.list().then(result => {
+        db.disconnect();
         expect(Array.isArray(result)).tobe(true);
-    })
-    it('listar usuários', async () => {
-        const result = await repo.insert({
-            nome: 'Jonathan',
-            email: 'jonathan@jonathan.com',
-            password: '1234'
-        });
-        expect(typeof result == 'object').tobe(true);
-    })
-    it('listar usuários', async () => {
-        const result = await repo.list();
-        expect(Array.isArray(result)).tobe(true);
-    })
-})
+        done();
+      });
+    // });
+  });
+
+  it("insert users", async () => {
+    repo
+      .insert({
+        nome: "Jonathan",
+        email: "jonathan@jonathan.com",
+        password: "1234"
+      })
+      .then(result => {
+        expect(typeof result == "object").tobe(true);
+        done();
+      });
+  });
+
+  it("listar usuários", async () => {
+    const result = await repo.list();
+    expect(Array.isArray(result)).tobe(true);
+  });
+});
